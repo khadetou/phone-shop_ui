8f4e6e919c99e68efa985d2a5910c6f067cf701a
khadetou <khadetou96@gmail.com>
2021-07-25 14:28:08 +0000

    floater finished

--- components/Floater.js
+++ components/Floater.js
@@ -1,8 +1,8 @@
 import styles from "../styles/style.module.scss";
 const Floater = () => {
-  const { floater } = styles;
+  const { floater, show } = styles;
   return (
-    <a href="#" className={floater}>
+    <a href="#" className={`${floater} ${show}`}>
       <svg>
         <use xlinkHref="/images/sprite.svg#icon-arrow-up"></use>
       </svg>
